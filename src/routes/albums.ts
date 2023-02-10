fcbd3e866dad8b9e9071f09689310d8e86e3c82a
christinen22 <christine.elin.louise@gmail.com>
2023-02-10 21:40:41 +0100

    Changes to routes

--- src/routes/albums.ts
+++ src/routes/albums.ts
@@ -4,4 +4,34 @@
 
 import express from 'express'
 import {body} from 'express-validator'
+import { index, show, store, update, destroy } from '../controllers/_controller'
 const router = express.Router()
+
+
+
+/**
+ * GET /resource
+ */
+router.get('/albums', index)
+
+/**
+ * GET /resource/:resourceId
+ */
+router.get('/albums/:almbumId', show)
+
+/**
+ * POST /resource
+ */
+router.post('/albums', [], store)
+
+/**
+ * PATCH /resource/:resourceId
+ */
+router.patch('/albums/:albumId', [], update)
+
+/**
+ * DELETE /resource/:resourceId
+ */
+router.delete('/album/:albumId', destroy)
+
+export default router
--- src/routes/photos.ts
+++ src/routes/photos.ts
@@ -2,6 +2,35 @@
  * Handles all photos
  */
 
+
 import express from 'express'
-import {body} from 'express-validator'
+import { body } from 'express-validator'
+import { index, show, store, update, destroy } from '../controllers/_controller'
 const router = express.Router()
+
+/**
+ * GET /resource
+ */
+router.get('/photos', index)
+
+/**
+ * GET /resource/:resourceId
+ */
+router.get('/photos/:photoId', show)
+
+/**
+ * POST /resource
+ */
+router.post('/photos', [], store)
+
+/**
+ * PATCH /resource/:resourceId
+ */
+router.patch('/photos/:photoId', [], update)
+
+/**
+ * DELETE /resource/:resourceId
+ */
+router.delete('/photos:photoId', destroy)
+
+export default router
