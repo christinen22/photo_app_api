a3baab2675dd59ee9bc33ddfd7e583036c8c3efc
christinen22 <christine.elin.louise@gmail.com>
2023-02-10 21:35:36 +0100

    Installed bcrypt, began with register controller

--- src/routes/users.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Handles all '/users' routes
- */
-
-import express, { Router } from 'express'
-import {body} from 'express-validator'
-const router = express.Router()
-
---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Type definitions
+ */
+
+export type CreateUserData = {
+    first_name: string,
+    last_name: string,
+    email: string
+}
