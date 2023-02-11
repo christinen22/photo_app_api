a883ceaa1b5560af52fd8132f2cb36817a788446
christinen22 <christine.elin.louise@gmail.com>
2023-02-11 14:29:00 +0100

    Added album and photo to types

--- src/services/album_services.ts
+++ src/services/album_services.ts
@@ -3,3 +3,28 @@
  */
 
 import prisma from '../prisma'
+import { CreateAlbumData } from '../types'
+
+
+
+export const getAlbum = async (albumId: number) => {
+    return await prisma.album.findUniqueOrThrow({
+        where: {
+            id: albumId,
+        },
+        include: {
+            photos: true
+        }
+    })
+}
+
+
+/*
+export const createAlbum = async (data: CreateAlbumData) => {
+    return await prisma.album.create({
+        data: {
+            title: data.title,
+        }
+    })
+}
+*/
--- src/types.d.ts
+++ src/types.d.ts
@@ -7,3 +7,14 @@ export type CreateUserData = {
     last_name: string,
     email: string
 }
+
+export type CreateAlbumData = {
+	title: string,
+}
+
+export type createPhotoData = {
+	title: string,
+	url: string,
+	albumId: number,
+	userId: number
+}
