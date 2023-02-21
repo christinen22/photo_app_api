50460f4efefc437c8c3dcb823ddc1d6c220ad3ba
christinen22 <christine.elin.louise@gmail.com>
2023-02-21 16:36:06 +0100

    Removed migrationfolder

--- prisma/migrations/20230212091518_albumid_photo/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `albumId` to the `Photo` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `photo` ADD COLUMN `albumId` INTEGER UNSIGNED NOT NULL;
--- prisma/migrations/20230214074817_relation/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `photo_id` on the `album` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `album` DROP COLUMN `photo_id`;
